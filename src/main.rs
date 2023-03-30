390251f53de050967b4a1a42e8913f94590a398a
Ananas77 <ananas7c@gmail.com>
2023-03-30 18:31:35 +0200

    Fixed number printing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod term;
 use crate::term::*;
 
 fn main() {
-    let my_number:Box<dyn Term> = Box::new(Number::new(3.0));
-    println!("hi {}", my_number.solve());
+	let my_number:Box<dyn Term> = Box::new(Number::new(3.0));
+	println!("hi {}", my_number.solve());
 }
--- src/term.rs
+++ src/term.rs
@@ -2,41 +2,42 @@ use std::fmt;
 
 pub trait Term
 {
-    fn solve(&self) -> Box<dyn Term> {Box::new(Number{value:0.0})}
+	fn solve(&self) -> Box<dyn Term> {Box::new(Number::new(0.0))} // returns the term's exact value
+	fn print(&self) -> String {"0".to_string()}                   // print the term
 }
 
 impl fmt::Display for dyn Term {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", 0.0)
-    }
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", self.print())
+	}
 }
 
 pub struct Number
 {
-    value:f32
+	value:f32
 }
 
 impl Term for Number
 {
-    fn solve(&self) -> Box<dyn Term>
-    {
-        Box::new(Number{value:self.value})
-    }
+	// returns value
+	fn solve(&self) -> Box<dyn Term>
+	{
+		Box::new(Number{value:self.value})
+	}
+
+	fn print(&self) -> String
+	{
+		self.value.to_string()
+	}
 }
 
 impl Number
 {
-    pub fn new(value:f32) -> Self
-    {
-        Number
-        {
-            value:value
-        }
-    }
-}
-
-impl fmt::Display for Number {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.value)
-    }
+	pub fn new(value:f32) -> Self
+	{
+		Number
+		{
+			value:value,
+		}
+	}
 }
