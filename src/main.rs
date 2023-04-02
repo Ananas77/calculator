cd6949e5782baf22abeac8b24820e215d7b5e52d
Ananas77 <ananas7c@gmail.com>
2023-04-02 10:36:41 +0200

    Remove unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,11 @@ mod math;
 
 use std::io;
 
-use math::prime_factors;
-use term::Number;
-
-use crate::{input::*, term::Term};
+use crate::{input::*};
 
 fn main() {
 	let mut input: String = String::new();
 	io::stdin().read_line(&mut input).expect("Failed to read input");
 	let term = term_from_string(input);
 	println!("{} = {}", term, term.calculate());
-	println!("{}, {}", prime_factors(Box::new(Number::new(20.0))).print(), Box::new(Number::new(2.0)).print());
 }
