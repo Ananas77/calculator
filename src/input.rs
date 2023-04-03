9ce1ef91c67979f9a35a900268a5200c1d0875e7
Ananas77 <ananas7c@gmail.com>
2023-04-03 19:48:05 +0200

    Update term_from_string

--- src/input.rs
+++ src/input.rs
@@ -1,53 +1,89 @@
 use crate::{term::*, sum::Sum, product::Product, variable::Variable, fraction::Fraction, power::Power};
 
-pub fn term_from_string(input: String) -> Box<dyn Term>
+pub fn term_from_string(input: &str) -> Result<Box<dyn Term>, String>
 {
-    let mut result: Box<dyn Term> = Box::new(Number::new(0.0));
-    let parts: Vec<String> = input.split_whitespace().map(|s| s.to_string()).collect();
-    let mut interpret_next_as: TermType = TermType::None;
-    for part in parts
-    {
-        if let Ok(i) = part.parse::<f64>()
-        {
-            match interpret_next_as
-            {
-                TermType::Sum => result = Box::new(Sum::new(vec![result, Box::new(Number::new(i))])),
-                TermType::Product => result = Box::new(Product::new(vec![result, Box::new(Number::new(i))])),
-                TermType::Fraction => result = Box::new(Fraction::new(result, Box::new(Number::new(i)))),
-                TermType::Power => result = Box::new(Power::new(result, Box::new(Number::new(i)))),
-                _ => result = Box::new(Number::new(i))
+    type Err = String;
+    let tokens = input.split_whitespace().collect::<Vec<_>>();
+    let mut iter = tokens.iter().peekable();
+    let term = parse_expr(&mut iter);
+    term
+}
+
+fn parse_expr(iter: &mut std::iter::Peekable<std::slice::Iter<&str>>) -> Result<Box<dyn Term>, String>
+{
+    let mut term = parse_term(iter)?;
+    while let Some(&op) = iter.peek() {
+        match op {
+            &"+" => {
+                iter.next();
+                let rhs = parse_term(iter)?;
+                term = Box::new(Sum::new(vec![term, rhs]));
             }
-            interpret_next_as = TermType::None;
-            continue;
-        }
-        if part == "+"
-        {
-            interpret_next_as = TermType::Sum;
-            continue;
-        }
-        if part == "*"
-        {
-            interpret_next_as = TermType::Product;
-            continue;
-        }
-        if part == "/"
-        {
-            interpret_next_as = TermType::Fraction;
-            continue;
-        }
-        if part == "^"
-        {
-            interpret_next_as = TermType::Power;
-            continue;
-        }
-        match interpret_next_as
-        {
-            TermType::Sum => result = Box::new(Sum::new(vec![result, Box::new(Variable::new(&part))])),
-            TermType::Product => result = Box::new(Product::new(vec![result, Box::new(Variable::new(&part))])),
-            TermType::Fraction => result = Box::new(Fraction::new(result, Box::new(Variable::new(&part)))),
-            TermType::Power => result = Box::new(Power::new(result, Box::new(Variable::new(&part)))),
-            _ => result = Box::new(Variable::new(&part))
+            &"-" => {
+                iter.next();
+                let rhs = parse_term(iter)?;
+                term = Box::new(Sum::new(vec![term, Box::new(Product::new(vec![Box::new(Number::new(-1.0)), rhs]))]));
+            }
+            _ => break,
         }
     }
-    result
+    Ok(term)
+}
+
+fn parse_term(iter: &mut std::iter::Peekable<std::slice::Iter<&str>>) -> Result<Box<dyn Term>, String> {
+    let mut term = parse_factor(iter)?;
+    while let Some(&op) = iter.peek() {
+        match op {
+            &"*" => {
+                iter.next();
+                let rhs = parse_factor(iter)?;
+                term = Box::new(Product::new(vec![term, rhs]));
+            }
+            &"/" => {
+                iter.next();
+                let rhs = parse_factor(iter)?;
+                term = Box::new(Fraction::new(term, rhs));
+            }
+            _ => break,
+        }
+    }
+    Ok(term)
+}
+
+fn parse_factor(iter: &mut std::iter::Peekable<std::slice::Iter<&str>>) -> Result<Box<dyn Term>, String> {
+    let mut term = parse_power_part(iter)?;
+    while let Some(&op) = iter.peek() {
+        match op {
+            &"^" => {
+                iter.next();
+                let rhs = parse_power_part(iter)?;
+                term = Box::new(Power::new(term, rhs));
+            }
+            _ => break,
+        }
+    }
+    Ok(term)
+}
+
+fn parse_power_part(iter: &mut std::iter::Peekable<std::slice::Iter<&str>>) -> Result<Box<dyn Term>, String> {
+    match iter.next().ok_or_else(|| "Unexpected end of input".to_string())? {
+        &"(" => {
+            let term = parse_expr(iter)?;
+            if iter.next() != Some(&")") {
+                return Err("Expected ')'".to_string());
+            }
+            Ok(term)
+        }
+        &"-" => {
+            let factor = parse_power_part(iter)?;
+            Ok(Box::new(Product::new(vec![Box::new(Number::new(-1.0)), factor])))
+        }
+        token => {
+            if let Ok(val) = token.parse() {
+                Ok(Box::new(Number::new(val)))
+            } else {
+                Ok(Box::new(Variable::new(token)))
+            }
+        }
+    }
 }
