06e9bac25946959049db11100c848c867d470d4b
Ananas77 <ananas7c@gmail.com>
2023-04-15 16:28:14 +0200

    Implement PartialEq and Eq for Term

--- src/term.rs
+++ src/term.rs
@@ -1,4 +1,5 @@
-use std::{fmt, vec};
+use core::{panic};
+use std::{fmt, vec, collections::HashMap, hash::{Hash, Hasher}};
 
 use crate::fraction::Fraction;
 
@@ -29,6 +30,95 @@ impl fmt::Display for dyn Term // for printing terms
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "{}", self.print())
 	}
+}
+
+impl PartialEq for dyn Term
+{
+	fn eq(&self, other: &Self) -> bool
+	{
+		match self.get_type()
+		{
+			TermType::Number => if other.get_type() == TermType::Number
+			{
+				self.get_value() == other.get_value()
+			}
+			else {
+				false
+			},
+			TermType::Variable => if other.get_type() == TermType::Variable
+			{
+				self.print() == other.print()
+			}
+			else {
+				false
+			},
+			TermType::Sum => if other.get_type() == TermType::Sum
+			{
+				let mut self_summands_hashmap = HashMap::new();
+				let mut other_summands_hashmap = HashMap::new();
+				for summand in self.get_parts()
+				{
+					*self_summands_hashmap.entry(summand).or_insert(0) += 1;
+				}
+				for summand in other.get_parts()
+				{
+					*other_summands_hashmap.entry(summand).or_insert(0) += 1;
+				}
+				self_summands_hashmap == other_summands_hashmap
+			}
+			else {
+				false
+			},
+			TermType::Product => if other.get_type() == TermType::Product
+			{
+				let mut self_factors_hashmap = HashMap::new();
+				let mut other_factors_hashmap = HashMap::new();
+				for summand in self.get_parts()
+				{
+					*self_factors_hashmap.entry(summand).or_insert(0) += 1;
+				}
+				for summand in other.get_parts()
+				{
+					*other_factors_hashmap.entry(summand).or_insert(0) += 1;
+				}
+				self_factors_hashmap == other_factors_hashmap
+			}
+			else {
+				false
+			},
+			TermType::Fraction => if other.get_type() == TermType::Fraction
+			{
+				(self.get_parts()[0] == other.get_parts()[0].copy()) & (self.get_parts()[1] == other.get_parts()[1].copy())
+			}
+			else {
+				false
+			},
+			TermType::Power => if other.get_type() == TermType::Power
+			{
+				(self.get_parts()[0] == other.get_parts()[0].copy()) & (self.get_parts()[1] == other.get_parts()[1].copy())
+			}
+			else {
+				false
+			},
+			_ => false
+		}
+	}
+}
+
+impl Clone for Box<dyn Term>
+{
+	fn clone(&self) -> Self {
+		self.copy()
+	}
+}
+
+impl Eq for dyn Term {}
+
+impl Hash for dyn Term
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.calculate(true).print().hash(state);
+    }
 }
 
 #[derive(Clone, Copy)]
